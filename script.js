fd250b52d8b6f0bf190d3666ba6642dcf544a1d3
Prashu2181 <prasannamadireddy466@gmail.com>
2025-02-17 20:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.querySelector("input").addEventListener("click",=>{
-	let dropDown =document.querySelector("colorSelect");
-	dropDown.remove(dropDown.selectedIndex);
-})
+const colorSelect = document.getElementById("colorSelect");
+const removeButton = document.querySelector("input[type='button']");
+
+removeButton.onclick = function() {
+    colorSelect.remove(colorSelect.selectedIndex);
+};
